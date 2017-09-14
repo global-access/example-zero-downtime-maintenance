8f2ccd6bf992d95d3d68f9268eb45ff7b2028523
Pavlo Kerestey <pavlo@kerestey.net>
2017-09-14 16:23:17 +0200

    Make sure the ghc compiler is installed when user runs any of the programs

--- watch-writing.hs
+++ watch-writing.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack script --resolver=lts-9.4 --package=bytestring --package=clock-extras --package=postgresql-simple
+-- stack script --install-ghc --resolver=lts-9.4 --package=bytestring --package=clock-extras --package=postgresql-simple
 
 import Example
 
